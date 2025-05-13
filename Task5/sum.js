ba197319ab053e9c1ba6d34f8d072a5e2a71d71a
NOCTRL5 <osamatawara7@gmail.com>
2025-05-13 15:45:37 +0300

    task 6

---
+++ Task5/sum.js
@@ -0,0 +1,10 @@
+function sum() {
+    let s = 0;
+    let arr = [];
+    for (let i = 0; i <= 100; i++){
+        arr.push(i);
+        if(i%2==0)
+            s += i;
+    }
+    alert("The sum is " + s);
+}
